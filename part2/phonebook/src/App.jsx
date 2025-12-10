e2dfe323aca02135cc8f9ccf5a8788bed8ecc019
Daniele Meca <mecaidlir@gmail.com>
2025-12-10 09:59:37 +0100

    completed phonebook exercises

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import personService from "./services/persons";
+import "./index.css";
+
+const capitalizeName = (name) => {
+  return name
+    .trim()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+};
 
 export default function App() {
   const [persons, setPersons] = useState([]);
+  const [message, setMessage] = useState(null);
+  const [category, setCategory] = useState("");
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -17,18 +28,81 @@ export default function App() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    !persons.some((person) => person.name === newPerson.name)
-      ? (setPersons([
-          ...persons,
-          { name: newPerson.name, number: newPerson.number },
-        ]),
-        setNewPerson({ name: "", number: "" }))
-      : alert(`${newPerson.name} is already added to phonebook`);
+    const capitalizedPerson = {
+      ...newPerson,
+      name: capitalizeName(newPerson.name),
+    };
+    !persons.some((person) => person.name === capitalizedPerson.name)
+      ? personService
+          .create(capitalizedPerson)
+          .then((returnedPerson) => {
+            setPersons(persons.concat(returnedPerson));
+            setNewPerson({ name: "", number: "" });
+            setCategory("added");
+            setMessage(`Added ${returnedPerson.name}`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            if (error.response.status === 404) {
+              setCategory("deleted");
+              setMessage(
+                `Information of ${capitalizedPerson.name} has already been removed from server`
+              );
+              setTimeout(() => {
+                setMessage(null);
+              }, 5000);
+            }
+          })
+      : personService
+          .update(
+            persons.find((p) => p.name === capitalizedPerson.name).id,
+            capitalizedPerson
+          )
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== returnedPerson.id ? person : returnedPerson
+              )
+            );
+            setNewPerson({ name: "", number: "" });
+            setCategory("updated");
+            setMessage(`Updated ${returnedPerson.name}`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            if (error.response.status === 404) {
+              setCategory("deleted");
+              setMessage(
+                `Information of ${capitalizedPerson.name} has already been removed from server`
+              );
+              setTimeout(() => {
+                setMessage(null);
+              }, 5000);
+            }
+          });
+  }
+
+  function handleDelete(id) {
+    const person = persons.find((p) => p.id === id);
+    window.confirm(`Delete ${person.name} ?`) &&
+      personService.deletePerson(id).then(() => {
+        setPersons(persons.filter((p) => p.id !== id));
+        setCategory("deleted");
+        setMessage(`Deleted ${person.name}`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      });
   }
 
   return (
     <div>
-      <h2>{`Phonebook`}</h2>filter shown with
+      <h2>{`Phonebook`}</h2>
+      <Message message={message} category={category} />
       <Filter filter={filter} setFilter={setFilter} />
       <h3>{`add a new`}</h3>
       <PersonForm
@@ -37,9 +111,17 @@ export default function App() {
         handleSubmit={handleSubmit}
       />
       <h3>{`Numbers`}</h3>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} handleDelete={handleDelete} />
     </div>
   );
+}
+
+function Message({ message, category }) {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className={`${category}`}>{message}</div>;
 }
 
 function Filter({ filter, setFilter }) {
@@ -80,12 +162,13 @@ function PersonForm({ newPerson, setNewPerson, handleSubmit }) {
   );
 }
 
-function Persons({ persons, filter }) {
+function Persons({ persons, filter, handleDelete }) {
   return persons.map((person) => {
     if (person.name.toLowerCase().includes(filter.toLowerCase()))
       return (
         <p key={person.name}>
-          {person.name} {person.number}
+          {person.name} {person.number}{" "}
+          <button onClick={() => handleDelete(person.id)}>{`delete`}</button>
         </p>
       );
   });
